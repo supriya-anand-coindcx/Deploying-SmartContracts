f081f4c408c8a173e0130653a1b5a56e5a2eaa24
supriya-anand-coindcx <supriya.anand@coindcx.com>
2022-09-01 12:59:09 +0530

    deploying the contract

--- deploy.js
+++ deploy.js
@@ -15,13 +15,13 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log('Deploying contract from account:', accounts[0]);
 
   const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi I am new to blockchain!'] })
     .send({ gas: '1000000', from: accounts[0] });
 
-  console.log('Contract deployed to', result.options.address);
+  console.log('Contract deployed to: ', result.options.address);
   provider.engine.stop();
 };
 deploy();
