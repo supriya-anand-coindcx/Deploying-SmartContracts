d672ece6f871e0a0147a55bc5c0fc9c893ae21b1
supriya-anand-coindcx <supriya.anand@coindcx.com>
2022-09-01 13:10:45 +0530

    put contents in .env

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config/config.env' });
 
 const { abi, evm } = require('./compile');
 
