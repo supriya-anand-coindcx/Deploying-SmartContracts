58afbf6a853f23239bc3b4d8a0e0d816631ba143
supriya-anand-coindcx <supriya.anand@coindcx.com>
2022-09-01 12:26:44 +0530

    deployed smart contracts

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  `${process.env.MNEMONIC}`,
+  `${process.env.INFURA_RINKBY_URI}`
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
